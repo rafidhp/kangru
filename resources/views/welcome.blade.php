eae664d82400b25374240f554e7b2629ec7bd740
rafidhp <rafiislamipasha27@gmail.com>
2025-07-08 14:00:03 +0700

    fix: tailwindcss conf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-    <h1>hello world!</h1>
+    <h1 class="text-[#ffffff]">hello world!</h1>
 </body>
 </html>
