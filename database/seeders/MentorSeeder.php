ecf06dd1e7e1727ba044ed4ef1aff87fc8c457eb
rafidhp <rafiislamipasha27@gmail.com>
2025-07-12 00:05:36 +0700

    chore: add dummy data

--- database/seeders/MentorSeeder.php
+++ database/seeders/MentorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mentor;
 use Illuminate\Database\Seeder;
 
 class MentorSeeder extends Seeder
@@ -13,13 +14,25 @@ class MentorSeeder extends Seeder
     {
         $mentors = [
             [
-                'no_hp' => '081234567890',
+                'no_hp' => '082134560099',
                 'pendidikan' => 'SMKN 1 Karawang',
-                'instansi' => 'Institut Teknologi Bandung',
-                'description' => 'Jaki is a very experienced mentor in the field of anime',
+                'instansi' => 'National University of Singapore',
+                'description' => 'mentor is very experienced, especially in the field of technology, so he can provide a lot of interesting inside for you',
                 'user_id' => 2,
                 'category_id' => 1,
             ],
+            [
+                'no_hp' => '081234567890',
+                'pendidikan' => 'MAN 2 SUbang',
+                'instansi' => 'Institut Teknologi Bandung',
+                'description' => 'Jaki is a very experienced mentor in the field of anime',
+                'user_id' => 4,
+                'category_id' => 1,
+            ],
         ];
+
+        foreach ($mentors as $mentor) {
+            Mentor::create($mentor);
+        }
     }
 }
