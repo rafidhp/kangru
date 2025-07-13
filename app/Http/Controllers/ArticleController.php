be921f897beae61846957031e0d30eea9b89adef
rafidhp <rafiislamipasha27@gmail.com>
2025-07-13 15:33:59 +0700

    fix: pint format

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
         $articles = Article::all();
+
         return view('article.index', compact('articles'));
     }
 
