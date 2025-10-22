12661258623fbc374c666e468d8f88b6bb380544
rafidhp <rafiislamipasha27@gmail.com>
2025-10-22 22:27:29 +0700

    chore: update server conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Jalankan migrasi
-# php artisan migrate:fresh --force
+php artisan migrate:fresh --force
 
-# Jalankan seeder (opsional)
-# php artisan db:seed --force || echo "Seeder error, lanjut ke Apache..."
+Jalankan seeder (opsional)
+php artisan db:seed --force || echo "Seeder error, lanjut ke Apache..."
 
-# php artisan storage:link
+php artisan storage:link
 
 php artisan config:clear
 php artisan cache:clear
